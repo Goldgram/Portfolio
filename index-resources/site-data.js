2ed6d4913c4a526ddb92fae89d8116be301cd1fa
Goldgram <willbrennandesign@gmail.com>
2019-12-04 00:49:58 +0000

    After designer tips

--- index-resources/site-data.js
+++ index-resources/site-data.js
@@ -5,7 +5,7 @@ const siteData = [
       src: "movies.jpg",
       url: "http://movies.willbrennan.tech/"
     },
-    text: "This is a small React project written in Typescript, using a The Movie Database API. Features a URL driven list controller and detailed item view.",
+    text: "A small React project written in Typescript, using a The Movie Database API. It features a URL driven list controller and detailed item view.",
     tags: [
       { name: "React", url: "https://reactjs.org/" },
       { name: "TypeScript", url: "https://www.typescriptlang.org/" },
@@ -22,7 +22,7 @@ const siteData = [
       src: "turtle.jpg",
       url: "https://github.com/Goldgram/TurtleChallenge"
     },
-    text: "This is a solution to a challenge that our team made up in eMaint. We used this to interview candidates for developer positions on our team. I created this version to show what a lean FP approach looks like.",
+    text: "A solution to a challenge that our team made up in eMaint. We used it to interview candidates for developer positions on our team. I created this version to show what a lean functional approach looks like.",
     tags: [
       { name: "Vanilla JS", url: "http://vanilla-js.com/" },
       { name: "Functional Programming", url: "https://en.wikipedia.org/wiki/Functional_programming" }
@@ -37,7 +37,7 @@ const siteData = [
       src: "kendo.jpg",
       url: "http://dublinkendo.com/"
     },
-    text: "A website made for the Dublin Kendo Club, with modern style in mind. Fully Responsive, Uses a JSON driven content approach, with Google Maps and Facebook integrations.",
+    text: "A website made for the Dublin Kendo Club, with modern style in mind. Fully Responsive, uses a JSON driven content approach, with Google Maps and Facebook integrations.",
     tags: [
       { name: "Parallax.js", url: "https://pixelcog.github.io/parallax.js/" },
       { name: "JQuery", url: "https://jquery.com/" },
@@ -54,7 +54,7 @@ const siteData = [
     image: {
       src: "euler.jpg"
     },
-    text: "Here is solutions to Project Euler problems in PHP and Javascript.",
+    text: "Solutions to Project Euler problems in both PHP and Javascript.",
     tags: [
       { name: "Project Euler", url: "https://projecteuler.net/" },
       { name: "PHP", url: "https://www.php.net/" },
@@ -71,7 +71,7 @@ const siteData = [
       src: "pokemon.jpg",
       url: "https://willbrennan.tech/png-mask/"
     },
-    text: 'This single page site highlights what the png mask algorithm does. Based on the "Who\'s that Pokemon?" trope, it maps a PNG and generates a SVG dynamically.',
+    text: 'A single page site that highlights what my png-mask algorithm does. Based on the "Who\'s that Pokemon?" trope, it maps a PNG and generates a SVG dynamically based on the outlines.',
     tags: [
       { name: "PNG", url: "https://en.wikipedia.org/wiki/Portable_Network_Graphics" },
       { name: "SVG", url: "https://en.wikipedia.org/wiki/Scalable_Vector_Graphics" },
@@ -89,7 +89,7 @@ const siteData = [
       src: "responsive.jpg",
       url: "https://willbrennan.tech/responsive-example/"
     },
-    text: "This is a responsive example based on a snapshot design. It is a one-page site with minimal complexity using a few front end libraries.",
+    text: "A responsive example based on a snapshot design. A one page site with minimal complexity using a few front end libraries.",
     tags: [
       { name: "Masonry.js", url: "https://masonry.desandro.com/" },
       { name: "LESS", url: "http://lesscss.org/" },
@@ -102,7 +102,7 @@ const siteData = [
   },
   {
     title: "This website!",
-    text: "Here is the repository for this site. It's very basic using no unnecessary libraries etc.",
+    text: "The repository for this site. It's very basic using no unnecessary libraries etc.",
     tags: [
       { name: "HTML", url: "https://en.wikipedia.org/wiki/HTML" },
       { name: "CSS", url: "https://en.wikipedia.org/wiki/Cascading_Style_Sheets" },
