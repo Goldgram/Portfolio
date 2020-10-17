8542b96e8e8ac611ef2c893a06007f14e7adfe5a
Goldgram <willbrennandesign@gmail.com>
2020-10-17 01:06:47 +0100

    Update content as live link changed, set up new link later

--- index-resources/site-data.js
+++ index-resources/site-data.js
@@ -3,130 +3,180 @@ const siteData = [
     title: "React Movie List",
     image: {
       src: "movies.jpg",
-      url: "http://movies.willbrennan.tech/"
+      url: "http://movies.willbrennan.tech/",
     },
-    text: "A small React project written in Typescript, using a The Movie Database API. It features a URL driven list controller and detailed item view.",
+    text:
+      "A small React project written in Typescript, using a The Movie Database API. It features a URL driven list controller and detailed item view.",
     tags: [
       { name: "React", url: "https://reactjs.org/" },
       { name: "TypeScript", url: "https://www.typescriptlang.org/" },
-      { name: "Functional Programming", url: "https://en.wikipedia.org/wiki/Functional_programming" }
+      {
+        name: "Functional Programming",
+        url: "https://en.wikipedia.org/wiki/Functional_programming",
+      },
     ],
     links: [
       { name: "Live Link", url: "http://movies.willbrennan.tech/" },
-      { name: "Code on Github", url: "https://github.com/Goldgram/ReactMovieList" }
-    ]
-  },
-  {
-    title: "Turtle Challenge",
-    image: {
-      src: "turtle.jpg",
-      url: "https://github.com/Goldgram/TurtleChallenge"
-    },
-    text: "A solution to a challenge that our team made up in eMaint. We used it to interview candidates for developer positions on our team. I created this version to show what a lean functional approach looks like.",
-    tags: [
-      { name: "Vanilla JS", url: "http://vanilla-js.com/" },
-      { name: "Functional Programming", url: "https://en.wikipedia.org/wiki/Functional_programming" }
+      {
+        name: "Code on Github",
+        url: "https://github.com/Goldgram/ReactMovieList",
+      },
     ],
-    links: [
-      { name: "Code on Github", url: "https://github.com/Goldgram/TurtleChallenge" }
-    ]
-  },
-  {
-    title: "Dublin Kendo Club",
-    image: {
-      src: "kendo.jpg",
-      url: "http://dublinkendo.com/"
-    },
-    text: "A website made for the Dublin Kendo Club, with modern style in mind. Fully Responsive, uses a JSON driven content approach, with Google Maps and Facebook integrations.",
-    tags: [
-      { name: "Parallax.js", url: "https://pixelcog.github.io/parallax.js/" },
-      { name: "JQuery", url: "https://jquery.com/" },
-      { name: "SCSS", url: "https://sass-lang.com/" },
-      { name: "PHP Mailer", url: "https://github.com/PHPMailer/PHPMailer" }
-    ],
-    links: [
-      { name: "Live Link", url: "http://dublinkendo.com/" },
-      { name: "Code on Github", url: "https://github.com/Goldgram/DublinKendo" }
-    ]
-  },
-  {
-    title: "Responsive Sample",
-    image: {
-      src: "responsive.jpg",
-      url: "https://willbrennan.tech/responsive-example/"
-    },
-    text: "A responsive example based on a snapshot design. A one page site with minimal complexity using a few front end libraries.",
-    tags: [
-      { name: "Masonry.js", url: "https://masonry.desandro.com/" },
-      { name: "LESS", url: "http://lesscss.org/" },
-      { name: "Bootstrap", url: "https://getbootstrap.com/" }
-    ],
-    links: [
-      { name: "Live Link", url: "https://willbrennan.tech/responsive-example/" },
-      { name: "Code on Github", url: "https://github.com/Goldgram/InterviewProjects#zg-interview-project" }
-    ]
   },
   {
     title: "Project Euler",
     image: {
-      src: "euler.jpg"
+      src: "euler.jpg",
     },
     text: "Solutions to Project Euler problems in both PHP and Javascript.",
     tags: [
       { name: "Project Euler", url: "https://projecteuler.net/" },
       { name: "PHP", url: "https://www.php.net/" },
-      { name: "JavaSrcipt", url: "https://www.javascript.com/" }
+      { name: "JavaSrcipt", url: "https://www.javascript.com/" },
     ],
     links: [
-      { name: "PHP Code on Github", url: "https://github.com/Goldgram/ProjectEulerPhp" },
-      { name: "Javascript Code on Github", url: "https://github.com/Goldgram/ProjectEulerJs" }
-    ]
+      {
+        name: "PHP Code on Github",
+        url: "https://github.com/Goldgram/ProjectEulerPhp",
+      },
+      {
+        name: "Javascript Code on Github",
+        url: "https://github.com/Goldgram/ProjectEulerJs",
+      },
+    ],
+  },
+  {
+    title: "This website!",
+    text:
+      "The repository for this site. It's very basic using no unnecessary libraries etc.",
+    tags: [
+      { name: "HTML", url: "https://en.wikipedia.org/wiki/HTML" },
+      {
+        name: "CSS",
+        url: "https://en.wikipedia.org/wiki/Cascading_Style_Sheets",
+      },
+      { name: "JavaScript", url: "https://www.javascript.com/" },
+    ],
+    links: [
+      { name: "Code on Github", url: "https://github.com/Goldgram/Portfolio" },
+    ],
   },
   {
     title: "Pokemon PNG Mask",
     image: {
       src: "pokemon.jpg",
-      url: "https://willbrennan.tech/png-mask/"
+      url: "https://willbrennan.tech/png-mask/",
     },
-    text: 'A single page site that highlights what my png-mask algorithm does. Based on the "Who\'s that Pokemon?" trope, it maps a PNG and generates a SVG dynamically based on the outlines.',
+    text:
+      'A single page site that highlights what my png-mask algorithm does. Based on the "Who\'s that Pokemon?" trope, it maps a PNG and generates a SVG dynamically based on the outlines.',
     tags: [
-      { name: "PNG", url: "https://en.wikipedia.org/wiki/Portable_Network_Graphics" },
-      { name: "SVG", url: "https://en.wikipedia.org/wiki/Scalable_Vector_Graphics" },
+      {
+        name: "PNG",
+        url: "https://en.wikipedia.org/wiki/Portable_Network_Graphics",
+      },
+      {
+        name: "SVG",
+        url: "https://en.wikipedia.org/wiki/Scalable_Vector_Graphics",
+      },
       { name: "Algorithm", url: "https://en.wikipedia.org/wiki/Algorithm" },
-      { name: "Pokemon", url: "https://bulbapedia.bulbagarden.net/wiki/Who%27s_That_Pok%C3%A9mon%3F" }
+      {
+        name: "Pokemon",
+        url:
+          "https://bulbapedia.bulbagarden.net/wiki/Who%27s_That_Pok%C3%A9mon%3F",
+      },
     ],
     links: [
       { name: "Live Link", url: "https://willbrennan.tech/png-mask/" },
-      { name: "Code on Github", url: "https://github.com/Goldgram/PngMask" }
-    ]
+      { name: "Code on Github", url: "https://github.com/Goldgram/PngMask" },
+    ],
   },
   {
     title: "Code Snippets",
     image: {
       src: "code-snippets.jpg",
-      url: "https://willbrennan.tech/code-snippets/"
+      url: "https://willbrennan.tech/code-snippets/",
     },
-    text: "A group of UI code snippets. This includes cross browser Checkboxes, Select drop-downs, and responsive YouTube embed examples.",
+    text:
+      "A group of UI code snippets. This includes cross browser Checkboxes, Select drop-downs, and responsive YouTube embed examples.",
     tags: [
       { name: "JQuery", url: "https://jquery.com/" },
-      { name: "Snippets", url: "https://en.wikipedia.org/wiki/Snippet_(programming)" },
+      {
+        name: "Snippets",
+        url: "https://en.wikipedia.org/wiki/Snippet_(programming)",
+      },
       { name: "YouTube", url: "https://www.youtube.com/" },
     ],
     links: [
       { name: "Live Link", url: "https://willbrennan.tech/code-snippets/" },
-      { name: "Code on Github", url: "https://github.com/Goldgram/Web-Snippets" }
-    ]
+      {
+        name: "Code on Github",
+        url: "https://github.com/Goldgram/Web-Snippets",
+      },
+    ],
   },
   {
-    title: "This website!",
-    text: "The repository for this site. It's very basic using no unnecessary libraries etc.",
+    title: "Responsive Sample",
+    image: {
+      src: "responsive.jpg",
+      url: "https://willbrennan.tech/responsive-example/",
+    },
+    text:
+      "A responsive example based on a snapshot design. A one page site with minimal complexity using a few front end libraries.",
     tags: [
-      { name: "HTML", url: "https://en.wikipedia.org/wiki/HTML" },
-      { name: "CSS", url: "https://en.wikipedia.org/wiki/Cascading_Style_Sheets" },
-      { name: "JavaScript", url: "https://www.javascript.com/" }
+      { name: "Masonry.js", url: "https://masonry.desandro.com/" },
+      { name: "LESS", url: "http://lesscss.org/" },
+      { name: "Bootstrap", url: "https://getbootstrap.com/" },
     ],
     links: [
-      { name: "Code on Github", url: "https://github.com/Goldgram/Portfolio" }
-    ]
-  }
+      {
+        name: "Live Link",
+        url: "https://willbrennan.tech/responsive-example/",
+      },
+      {
+        name: "Code on Github",
+        url:
+          "https://github.com/Goldgram/InterviewProjects#zg-interview-project",
+      },
+    ],
+  },
+  {
+    title: "Turtle Challenge",
+    image: {
+      src: "turtle.jpg",
+      url: "https://github.com/Goldgram/TurtleChallenge",
+    },
+    text:
+      "A solution to a challenge that our team made up in eMaint. We used it to interview candidates for developer positions on our team. I created this version to show what a lean functional approach looks like.",
+    tags: [
+      { name: "Vanilla JS", url: "http://vanilla-js.com/" },
+      {
+        name: "Functional Programming",
+        url: "https://en.wikipedia.org/wiki/Functional_programming",
+      },
+    ],
+    links: [
+      {
+        name: "Code on Github",
+        url: "https://github.com/Goldgram/TurtleChallenge",
+      },
+    ],
+  },
+  // {
+  //   title: "Dublin Kendo Club",
+  //   image: {
+  //     src: "kendo.jpg",
+  //     url: "http://dublinkendo.com/"
+  //   },
+  //   text: "A website made for the Dublin Kendo Club, with modern style in mind. Fully Responsive, uses a JSON driven content approach, with Google Maps and Facebook integrations.",
+  //   tags: [
+  //     { name: "Parallax.js", url: "https://pixelcog.github.io/parallax.js/" },
+  //     { name: "JQuery", url: "https://jquery.com/" },
+  //     { name: "SCSS", url: "https://sass-lang.com/" },
+  //     { name: "PHP Mailer", url: "https://github.com/PHPMailer/PHPMailer" }
+  //   ],
+  //   links: [
+  //     { name: "Live Link", url: "http://dublinkendo.com/" },
+  //     { name: "Code on Github", url: "https://github.com/Goldgram/DublinKendo" }
+  //   ]
+  // },
 ];
